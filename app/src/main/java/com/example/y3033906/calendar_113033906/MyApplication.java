476404f9c5e1630312c2b84dc7b33853ff7922a5
TatsuyaHasunuma0123 <tatsuya_0123@icloud.com>
2021-06-15 20:50:43 +0900

    HomeFragment でTronald Dumpを表示

--- app/src/main/java/com/example/y3033906/calendar_113033906/MyApplication.java
+++ app/src/main/java/com/example/y3033906/calendar_113033906/MyApplication.java
@@ -2,6 +2,12 @@ package com.example.y3033906.calendar_113033906;
 
 import android.app.Application;
 import android.content.Context;
+import android.util.Log;
+import com.twitter.sdk.android.core.DefaultLogger;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterConfig;
+
 
 public class MyApplication extends Application {
     private static Context context;
@@ -9,6 +15,7 @@ public class MyApplication extends Application {
     public void onCreate(){
         super.onCreate();
         MyApplication.context = getApplicationContext();
+
     }
 
     public static Context getAppContext(){
--- app/src/main/java/com/example/y3033906/calendar_113033906/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/y3033906/calendar_113033906/ui/home/HomeFragment.java
@@ -1,18 +1,27 @@
 package com.example.y3033906.calendar_113033906.ui.home;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.y3033906.calendar_113033906.MainActivity;
 import com.example.y3033906.calendar_113033906.R;
+
+import static java.security.AccessController.getContext;
 
 public class HomeFragment extends Fragment {
 
@@ -23,13 +32,12 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        /*WebView myWebView = root.findViewById(R.id.webView);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("https://www.tronalddump.io/");*/
+
+
+
         return root;
     }
 }
