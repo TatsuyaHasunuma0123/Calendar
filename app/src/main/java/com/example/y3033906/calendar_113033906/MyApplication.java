f8ea1b9b5f9ed7038af0f5ede1097b9ccd06f9fc
TatsuyaHasunuma0123 <tatsuya_0123@icloud.com>
2021-06-16 13:12:06 +0900

    TronaldDump からJsonデータを取得

--- app/src/main/java/com/example/y3033906/calendar_113033906/MyApplication.java
+++ app/src/main/java/com/example/y3033906/calendar_113033906/MyApplication.java
@@ -3,10 +3,6 @@ package com.example.y3033906.calendar_113033906;
 import android.app.Application;
 import android.content.Context;
 import android.util.Log;
-import com.twitter.sdk.android.core.DefaultLogger;
-import com.twitter.sdk.android.core.Twitter;
-import com.twitter.sdk.android.core.TwitterAuthConfig;
-import com.twitter.sdk.android.core.TwitterConfig;
 
 
 public class MyApplication extends Application {
@@ -15,7 +11,6 @@ public class MyApplication extends Application {
     public void onCreate(){
         super.onCreate();
         MyApplication.context = getApplicationContext();
-
     }
 
     public static Context getAppContext(){
--- app/src/main/java/com/example/y3033906/calendar_113033906/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/y3033906/calendar_113033906/ui/home/HomeFragment.java
@@ -1,43 +1,116 @@
 package com.example.y3033906.calendar_113033906.ui.home;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.y3033906.calendar_113033906.MainActivity;
 import com.example.y3033906.calendar_113033906.R;
 
-import static java.security.AccessController.getContext;
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    TextView textView;
+    Button button;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        /*WebView myWebView = root.findViewById(R.id.webView);
-        myWebView.setWebViewClient(new WebViewClient());
-        myWebView.loadUrl("https://www.tronalddump.io/");*/
-
-
-
+        textView = root.findViewById(R.id.textView);
+        button = root.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    //httpRequest("http://www.bluecode.jp/test/api.php");
+                    httpRequest("https://api.tronalddump.io/random/quote");
+                } catch (Exception e) {
+                    Log.e("Hoge", e.getMessage());
+                }
+            }
+        });
         return root;
     }
+
+    void httpRequest(String url) throws IOException{
+
+        //OkHttpClinet生成
+        OkHttpClient client = new OkHttpClient();
+
+        //request生成
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        //final OkHttpClient client = new OkHttpClient.Builder().build();
+        //final Response response = client.newCall(request).execute();
+        //final String resultStr = response.body().string();
+
+        //非同期リクエスト
+        client.newCall(request)
+                .enqueue(new Callback() {
+
+                    //エラーのとき
+                    @Override
+                    public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                        Log.e("Hoge",e.getMessage());
+                    }
+
+                    //正常のとき
+                    @Override
+                    public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+
+                        //response取り出し
+                        final String jsonStr = response.body().string();
+                        Log.d("Hoge","jsonStr=" + jsonStr);
+
+                        //JSON処理
+                        try{
+                            //jsonパース
+                            JSONObject json = new JSONObject(jsonStr);
+                            final String value = json.getString("status");
+
+                            //親スレッドUI更新
+                            Handler mainHandler = new Handler(Looper.getMainLooper());
+                            mainHandler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    textView.setText(value);
+                                }
+                            });
+
+                        }catch(Exception e){
+                            Log.e("Hoge",e.getMessage());
+                        }
+                    }
+                });
+    }
 }
+
+
