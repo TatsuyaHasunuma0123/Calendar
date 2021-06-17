e04cbd0ab6b1c1eb15bb75e5270550130d65b80e
TatsuyaHasunuma0123 <tatsuya_0123@icloud.com>
2021-06-17 13:17:43 +0900

    翻訳APIにリクエストを送信

--- app/src/main/java/com/example/y3033906/calendar_113033906/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/y3033906/calendar_113033906/ui/home/HomeFragment.java
@@ -2,6 +2,7 @@ package com.example.y3033906.calendar_113033906.ui.home;
 
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -13,6 +14,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -29,12 +31,14 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Headers;
+import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -43,7 +47,7 @@ import okhttp3.Response;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-    TextView textView;
+    TextView textView,textView2;
     Button button;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -52,6 +56,7 @@ public class HomeFragment extends Fragment {
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         textView = root.findViewById(R.id.textView);
+        textView2 = root.findViewById(R.id.textView2);
         button = root.findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
 
@@ -68,7 +73,7 @@ public class HomeFragment extends Fragment {
         return root;
     }
 
-    void httpRequest(String url) throws IOException{
+    public void httpRequest(String url) throws IOException{
 
         //OkHttpClinet生成
         OkHttpClient client = new OkHttpClient();
@@ -102,6 +107,7 @@ public class HomeFragment extends Fragment {
                             //jsonパース
                             JSONObject json = new JSONObject(jsonStr);
                             final String value = json.getString("value");
+                            translate(value);
 
                             //親スレッドUI更新
                             Handler mainHandler = new Handler(Looper.getMainLooper());
@@ -109,6 +115,68 @@ public class HomeFragment extends Fragment {
                                 @Override
                                 public void run() {
                                     textView.setText(value);
+                                }
+                            });
+
+                        }catch(Exception e){
+                            Log.e("Hoge",e.getMessage());
+                        }
+                    }
+                });
+    }
+
+    private void translate(String value){
+        //OkHttpClinet生成
+        OkHttpClient client = new OkHttpClient();
+        String url ="https://script.google.com/macros/s/AKfycbzuDmbOrcwJBrUtvz8bQ13rRR4BZgYdjPy3850B089425l5AZ3D-yV1LiVarQYrkmp5Zw/exec";
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(url).newBuilder();
+
+        //set Request parameters
+        Map<String, String> params = new HashMap<>();
+        params.put("text", value);
+        params.put("source", "ja");
+        params.put("target", "ja");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            params.forEach(urlBuilder::addQueryParameter);
+        }
+
+        //request生成
+        Request request = new Request.Builder()
+                .url(url)
+                .addHeader("Accept", "*/*")
+                .build();
+
+        //非同期リクエスト
+        client.newCall(request)
+                .enqueue(new Callback() {
+
+                    //エラーのとき
+                    @Override
+                    public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                        Log.e("Hoge",e.getMessage());
+                    }
+
+                    //正常のとき
+                    @Override
+                    public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+
+                        //response取り出し
+                        final String jsonStr = response.body().string();
+                        Log.d("Hoge","jsonStr=" + jsonStr);
+
+                        //JSON処理
+                        try{
+                            //jsonパース
+                            JSONObject json = new JSONObject(jsonStr);
+                            //final String value = json.getString("value");
+                            //translate(value);
+
+                            //親スレッドUI更新
+                            Handler mainHandler = new Handler(Looper.getMainLooper());
+                            mainHandler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    //textView.setText(value);
                                 }
                             });
 
