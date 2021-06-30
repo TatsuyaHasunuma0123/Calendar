c9e8199b1a6422bdf21e8cd0bb7536f875f87ede
TatsuyaHasunuma0123 <tatsuya_0123@icloud.com>
2021-06-30 17:14:13 +0900

    追加：getSystemService

--- app/src/main/java/com/example/y3033906/calendar_113033906/MainActivity.java
+++ app/src/main/java/com/example/y3033906/calendar_113033906/MainActivity.java
@@ -2,8 +2,7 @@ package com.example.y3033906.calendar_113033906;
 
 import android.content.Context;
 import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.view.inputmethod.InputMethodManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -11,12 +10,17 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static InputMethodManager inputMethodManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inputMethodManager =  (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
