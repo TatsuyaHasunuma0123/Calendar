6523b0f01597a85b27d0f37ff71ff21b8d24ad7b
TatsuyaHasunuma0123 <tatsuya_0123@icloud.com>
2021-06-30 12:59:15 +0900

    修正：Button → ImageButton

--- app/src/main/java/com/example/y3033906/calendar_113033906/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/y3033906/calendar_113033906/ui/notifications/NotificationsFragment.java
@@ -5,29 +5,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.y3033906.calendar_113033906.MainActivity;
 import com.example.y3033906.calendar_113033906.MyApplication;
 import com.example.y3033906.calendar_113033906.R;
 import com.example.y3033906.calendar_113033906.ui.notifications.calendar.CalendarAdapter;
 import com.example.y3033906.calendar_113033906.ui.notifications.calendar.DateManager;
-
-import java.util.Arrays;
-import java.util.List;
-
-import soup.neumorphism.NeumorphCardView;
 
 public class NotificationsFragment extends Fragment {
 
@@ -45,7 +36,7 @@ public class NotificationsFragment extends Fragment {
                 new ViewModelProvider(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
         titleText = root.findViewById(R.id.titleText);
-        Button prevButton = root.findViewById(R.id.prevButton);
+        ImageButton prevButton = root.findViewById(R.id.prevButton);
         prevButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,7 +44,7 @@ public class NotificationsFragment extends Fragment {
                 titleText.setText(mCalendarAdapter.getTitle());
             }
         });
-        Button nextButton = root.findViewById(R.id.nextButton);
+        ImageButton nextButton = root.findViewById(R.id.nextButton);
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
