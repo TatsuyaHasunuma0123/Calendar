5e134fabd2085004c3ab0a749240b888911f9733
TatsuyaHasunuma0123 <tatsuya_0123@icloud.com>
2021-07-20 20:07:07 +0900

    追加：sreenNameからuserID取得

--- app/src/main/java/com/example/y3033906/calendar_113033906/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/y3033906/calendar_113033906/ui/dashboard/DashboardFragment.java
@@ -4,13 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.y3033906.calendar_113033906.R;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.User;
 
 public class DashboardFragment extends Fragment {
 
@@ -22,9 +27,30 @@ public class DashboardFragment extends Fragment {
                 new ViewModelProvider(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
 
-        WebView myWebView = root.findViewById(R.id.webView);
-        myWebView.loadUrl("https://idtwi.com/");
-
+        Button button = root.findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                android.os.AsyncTask<Void, Void, String> task
+                        = new android.os.AsyncTask<Void, Void, String>() {
+                    @Override
+                    protected String doInBackground(Void... params) {
+                        String word = "twitter4jを削除";
+                        String screenName = "ariyoshihiroiki";
+                        Twitter twitter = TwitterFactory.getSingleton();
+                        try {
+                            User user = twitter.verifyCredentials();
+                            user = twitter.showUser(screenName);
+                            System.out.println(user.getId());
+                        } catch (TwitterException e) {
+                            e.printStackTrace();
+                        }
+                        return null;
+                    }
+                };
+                task.execute();
+            }
+        });
         return root;
     }
 }
